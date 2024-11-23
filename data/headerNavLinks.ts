381eda365ed443b8e83927bfd9b761e8b21aec50
Xuanyu Chen <150888349+lava-chen@users.noreply.github.com>
2024-11-23 15:07:55 +0800

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: "/", title: "Home" },
   { href: "/blog", title: "Blog" },
-  { href: "https://lavachen.gitbook.io/lava-note", title: "Notes" },
+  { href: "https://notes.lavachen.org", title: "Notes" },
   { href: "/projects", title: "Projects" },
   { href: "/about", title: "About" },
 ];
